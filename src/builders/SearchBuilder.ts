02a553f37d0a240a298afe60f4ecef6483e42e8d
Diego Barros <diegobarros2812@gmail.com>
2023-03-01 22:28:49 -0400

    Adjusting SearchBuilder

--- src/builders/SearchBuilder.ts
+++ src/builders/SearchBuilder.ts
@@ -1,17 +1,17 @@
-import { Brackets } from 'typeorm';
-import { FilterOperator } from '../enums';
-import { FilterQuery } from '../types';
-import { Builder } from './Builder';
+import { Brackets } from "typeorm";
+import { FilterOperator } from "../enums";
+import { FilterQuery } from "../types";
+import { Builder } from "./Builder";
 
 export class SearchBuilder<T> extends Builder<T> {
-  build({ s }: FilterQuery<T>) {
+  build({ s, searchColumns }: FilterQuery<T>) {
     const { searchableColumns } = this.configuration;
 
-    if (s && this.configuration.searchableColumns?.length) {
+    if (s && searchColumns?.length) {
       this.queryBuilder.andWhere(
         new Brackets((qb) => {
-          for (const searchColumn of searchableColumns) {
-            if (!searchableColumns.includes(searchColumn)) {
+          for (const searchColumn of searchColumns) {
+            if (searchableColumns?.length && !searchableColumns.includes(searchColumn)) {
               continue;
             }
 
